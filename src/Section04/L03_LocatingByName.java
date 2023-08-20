433bbdcddbfe23e90ab0ebf6ea5a1bddfdd21c42
Brach <agata.brach@capgemini.com>
2023-08-20 20:03:11 +0200

    Initial Commit

---
+++ src/Section04/L03_LocatingByName.java
@@ -0,0 +1,24 @@
+package Section04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L03_LocatingByName {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abrach\\Desktop\\resources\\driver_v1\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();	
+	
+	driver.manage().window().maximize();
+	
+	driver.get("https://www.automationtesting.co.uk/contactForm.html");
+	
+	driver.findElement(By.linkText("Aktions")).click();
+	
+	}
+
+}
---
+++ src/Section05/L02_sendingData.java
@@ -0,0 +1,22 @@
+package Section05;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L02_sendingData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abrach\\Desktop\\resources\\driver_v1\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();	
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.automationtesting.co.uk/buttons.html");
+		
+		//driver.findElement(By.cssSelector("button#btn_three")).clink();
+	}
+
+}
